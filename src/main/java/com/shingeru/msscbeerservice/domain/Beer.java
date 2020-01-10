31ec3d28f18f7a3f83edbb101f9e02b88328a4cd
Norbert Kapkowski <norbertkapkowski@gmail.com>
2020-01-10 09:44:21 +0100

    jpa added

---
+++ src/main/java/com/shingeru/msscbeerservice/domain/Beer.java
@@ -0,0 +1,49 @@
+package com.shingeru.msscbeerservice.domain;
+
+import com.shingeru.msscbeerservice.web.model.BeerStyleEnum;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+    private String beerName;
+    private BeerStyleEnum beerStyle;
+
+    @Column(unique = true)
+    private Long upc;
+    private BigDecimal price;
+
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+
+}
