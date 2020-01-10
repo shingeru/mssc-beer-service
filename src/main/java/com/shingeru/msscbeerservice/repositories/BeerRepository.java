ded013c50effa89bdb2443cc04395e93b7079d17
Norbert Kapkowski <norbertkapkowski@gmail.com>
2020-01-10 09:48:17 +0100

    added beer repository

---
+++ src/main/java/com/shingeru/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,9 @@
+package com.shingeru.msscbeerservice.repositories;
+
+import com.shingeru.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+import java.util.UUID;
+
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
